fd29a79b00eb9fcd75bf2963076081a91379d832
Tracie Masek <traciemasek@gmail.com>
2019-06-01 13:02:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0 
+    "FizzBuzz"
   
 end
